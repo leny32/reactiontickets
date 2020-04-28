66be49f5f7f1112c239007c0d5c3af125c8b7282
leny32 <40176071+yezzl@users.noreply.github.com>
2020-04-28 12:47:34 +0200

    fix

---
+++ commands/eval.js
@@ -0,0 +1,33 @@
+const Reactions = require("../models/reactions");
+const Tickets = require("../models/tickets");
+const config = require("../config");
+
+exports.run = async (client, message, args) => {
+
+    if (!config.admins.includes(message.author.id)) return;
+
+    if (!args[0]) return message.channel.send("Missing arguments!")
+    const code = args.join(" ");
+    try {
+      const evaluat = async (c) => eval(c);
+      const evaled = await evaluat(code);
+      const clean = await client.clean(client, evaled);
+  
+      const MAX_CHARS = 3 + 2 + clean.length + 3;
+      if (MAX_CHARS > 1500) {
+        return message.channel.send("Output :", {
+          files: [{
+            attachment: Buffer.from(clean),
+            name: "eval.txt"
+          }]
+        });
+      }
+  
+      message.channel.send(`Input:\`\`\`js\n${code}\n\`\`\`\nOutput:\`\`\`js\n${clean}\n\`\`\``);
+    } catch (err) {
+      message.channel.send(`Input:\`\`\`js\n${code}\n\`\`\`\nOutput:\`\`\`xl\n${err}\n\`\`\``);
+    }
+  
+  
+  
+  }
