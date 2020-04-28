e0145428d3a0569fed0c85fcc5cead3be6bcedf9
leny32 <40176071+yezzl@users.noreply.github.com>
2020-04-28 16:34:55 +0200

    fix

--- commands/invite.js
+++ commands/invite.js
@@ -8,6 +8,8 @@ exports.run = async (client, guild, message, args) => {
     });
 
     const embed = new Discord.MessageEmbed()
+    .setTitle("Reaction Tickets | Invite")
+    .setURL("https://store.droplet.gg/reactiontickets")
     .setDescription("It is suggested to invite the bot with administrator permissions in order to bypass all permissions checks. This means that you only have to move the role over the role you are giving to make it work. Or you could use the normal invite that should have all needed permissions but does not bypass channel and other permissions.")
     .addField("Admin Invite", "Click [here](https://discordapp.com/oauth2/authorize?client_id=702920439131013190&permissions=8&scope=bot) to invite or url below\nhttps://discordapp.com/oauth2/authorize?client_id=702920439131013190&permissions=8&scope=bot")
     .addField("Normal Invite", "Click [here](https://discordapp.com/oauth2/authorize?client_id=702920439131013190&scope=bot&permissions=1342565456) to invite or url below\nhttps://discordapp.com/oauth2/authorize?client_id=702920439131013190&scope=bot&permissions=1342565456")
